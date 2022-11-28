9b837c0b0831a74670dc573fbb4e2047e486a1bf
rubytripathi23 <rubytripathi108@gmail.com>
2022-11-28 13:28:05 +0530

    leaning javascript

---
+++ index.js
@@ -0,0 +1,287 @@
+// // var name = 'Sajid Ahmad';
+// // var age = 25;
+// // console.log(name);
+// // console.log(age);
+
+// // var name = 'ruby tripathi'
+// // console. log(name);
+// // var age = 25;
+// // console.log(age)
+// // console. log(age);
+// // var iAmRuby = false;
+// // console.log(iAmRuby);
+
+// //  console. log(typeof(iAmRuby));
+
+// //  console. log( 10 + "20")
+
+// // console. log( 9 - "5");
+// // console. log( "java "         + "script");
+// // console. log(" " + 0);
+// // console. log("ruby" - "thapa");
+ 
+// // console. log( true + false);
+// // var iAmUseless = null;
+// // console. log(iAmUseless)
+
+// // var iAmUseless;
+
+// // var iAmUseless = null;
+// // console. log(iAmUseless);
+// //  console .log(typeof(iAmUseless));
+
+// // var myPhoeNumber = 9856456356;
+// // var myName = "ruby tripathi"
+
+// //  console. log(myPhoeNumber);
+// //  console. log(myName);
+
+// //  if(isNaN(myName)){
+// //     console. log("plz enter valid phone no");
+// //  }
+// //  var x = 5;
+// //  var y = 5;
+
+// //  console .log("is both the x and y are equal or not" + x == y );
+// // console. log(x == y);
+
+// // console. log(3+3);
+// // console. log(10-5);
+// // console. log(20/5);
+// // console. log(5*6);
+// //  console. log("Remainder operator " + 27%4);
+// // var num = 15;
+// // var newNum = --num + 5;
+// // console. log(num);
+// // console.log(newNum);
+
+// // var a = 30;
+// // var b = 10;
+
+// // console. log(a  != b);
+
+// // console. log(a > b);
+// // console. log("hello world");
+// // console. log("hello " + "world")
+ 
+// // console. log(5 - "ruby");
+// // var a = 5;
+// // var b = 10;
+
+// // var c = b;  
+// // b = a;
+// // a = c;
+
+// // console. log("the value of a is" + a);
+// // console. log("the value of b is" + b);
+ 
+// var a = 5;
+// var b = 10;
+
+// a = a + b;  a = 15;
+// b = a - b;  b = 5;
+// a = a - b;  a= 10;
+
+// console. log("the value of a is" + a);
+// console. log("the value of b is" + b);
+//  var num1 = 5;
+//  var num2 = '5';
+  
+//  console. log(typeof(num1));
+//  console. log(typeof(num2));
+
+//  console .log(num1 == num2 );
+ 
+// 
+// var year = 2020;
+
+// if(year % 4 === 0){
+//     if(year % 100 === 0){
+//         if(year % 400 === 0){
+//             console.log(" The year" + year + "is a leap year");
+//         }else{
+//             console. log("The year" + year + "is not a leap year");
+//         }
+//     }else{
+//         console. log("The year" + year + "  is a leap year");
+//     }  
+
+//     }else{
+//         console. log("the year" + year + "is  not a leap year");
+//     }
+// var area = "square"
+// var PI = 3.142, l=5, b=4, r=3;
+
+// if(area == "circle"){
+//     console. log("the area of the circle is : " + PI*r**2);
+// }else if(area == "triangle"){
+//     console. log("the area of the rectangle is : " + (l*b/2));
+// }else if(area == "rectangle"){
+//     console.log("the area of the rectangle is : " + (l*b));
+// }else{
+//     console. log("please entre valid data");
+// }
+
+// var area = "circle";
+// var PI = 3.142, l=5, b=4, r=3;
+
+// switch(area){
+//     case 'circle':
+//         console. log("the area of the circle is : " + PI*r**2);
+//         break;
+
+//    case 'triangle':
+//     console. log("the area of the rectangle is : " + (l*b/2));
+
+//    case 'rectangle':
+//     console.log("the area of the rectangle is : " + (l*b));
+
+//     default:
+//         console. log("please entre valid data");
+
+// }
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+
+// for(var num = 1; num <= 20; num++){
+//     var tableof = 8;
+//     console.log(tableof  + " * " + num + " = " + tableof * num);
+// }
+// function sum(){
+// var a = 10, b = 40;
+// var total = a+b;
+// console. log(total);
+// }
+// function sum(a,b){   //// sum(a,b)==parameter
+//     var total = a+b;    /// logitic witten
+//     console.log(total); //output
+// }
+// sum(20,30);//function call--argument pass 20,30
+
+
+// function empName(age){
+    
+// }
+// empName(30);
+// var age =19;
+// if(age<=19){
+//     console.log('you are allowed to vate');
+// }
+// else{
+//     console.log('you are not allowed to vote');
+// }
+// function vote(ages){
+// var age =ages;
+// if(age>=19){
+//      console.log('you are allowed to vate');
+//     }
+//  else{
+//     console.log('you are not allowed to vote
+
+//  var funExp = function(a,b){
+//    return total = a+b;
+// }
+// var sum = funExp = (15,15);
+// var sum1 = funExp = (20,15);
+
+
+//  console. log(sum > sum1 )
+//  
+// for(let num = 1; num <=10; num++){
+// let tableOf = 12;
+//  console. log( ` ${tableOf} * ${num} = ${6 + 6}` );
+// }
+
+// function mult(a,b=5){
+//   return a*b;
+// }
+
+// console. log(mult(3));
+
+// console.log(sum());
+
+// function sum() {
+//  let a = 5; b = 6;
+//  let sum = a+b;
+//  return`the sum of the number is ${sum}`;
+// }
+
+// const sum = () => `the sum of the two number is ${(a=5)+(b=6)}`;
+
+//  console.log(sum());
+
+
+// var myFriends = ['vinod','ramesh','arjun','vishal','vinod','ramesh','arjun','vishal','ruby','shivai','kripa','vinod','ramesh','arjun','vishal'];
+
+// console.log(myFriends[5]);
+
+
+// var myFriends = ['vinod','ramesh','arjun','vishal'];
+ 
+//     myFriends.forEach(function(element, index, array) {
+
+//         console.log(element + " index : " + index  + " " + array);
+
+//     });
+
+// const animals = ['pigs','goats','sheep'];
+
+// animals.count = animals.push('chicken');
+// console.log(animals);
+// console.log(count);
+
+//  animals.unshift('chicken','cats','cow');
+//  console.log(animals);
+ 
+// const myNumbers = [1,2,3,5,7,8];
+// myNumbers.unshift(4,6);
+
+// console.log(myNumbers);
+
+// const months = ['jan','march','april','june','july'];
+
+// const indexOfMonth = months.indexOf('March');
+// // console.log(indexOfMonth);
+
+// if(indexOfMonth !== -1){
+//     const updateMonths = months.splice(indexOfMonth,1,'March');
+//     console.log(updateMonths);
+
+// }
+// else{
+// console.log('no such a deta found');
+
+// }
+const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+let newFruits=fruits.splice(2, 2);
+console.log(newFruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
